f51d4b83e43860bbbb916c07899f198eadb19d9d
vbbandeira <vbritobandeira@gmail.com>
2022-03-02 16:42:23 -0300

    update roll the dice

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello World!");
+//Console.WriteLine("Hello World!");
+
 Random dice = new Random();
 
 int roll1 = dice.Next(1, 7);
@@ -12,16 +13,21 @@ Console.WriteLine($"Dice roll: {roll1} + {roll2} + {roll3} = {total}");
 
 if ((roll1 == roll2) || (roll2 == roll3) || (roll1 == roll3))
 {
-    Console.WriteLine("You rolled doubles! +2 bonus to total!");
+    Console.WriteLine("You rolled doubles!  +2 bonus to total!");
     total += 2;
+}
+
+if ((roll1 == roll2) && (roll2 == roll3))
+{
+    Console.WriteLine("You rolled triples!  +6 bonus to total!");
+    total += 6;
 }
 
 if (total >= 15)
 {
     Console.WriteLine("You win!");
 }
-
-if (total < 15)
+else
 {
     Console.WriteLine("Sorry, you lose.");
 }
